ad420078b87e9555bc0a11d7982002496b1d708a
yasshp <yassh.s.patel@gmail.com>
2026-01-29 16:18:00 +0530

    Add API URL debug logging

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -7,6 +7,8 @@ const api = axios.create({
   },
 });
 
+console.log('API Base URL:', api.defaults.baseURL); // Debug log to see where requests are going
+
 // Add token to every request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
